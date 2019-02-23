c05c887f7aa9342c708c7905766c583ffa008ca6
John Siirola <jsiirola@users.noreply.github.com>
2019-02-22 22:03:37 -0700

    Adding z3-solver; refactoring how we manage Python packages

--- linux_install_scripts/python_libs.sh
+++ linux_install_scripts/python_libs.sh
@@ -3,14 +3,22 @@ RUN echo "" && \
     echo "INSTALLING PYTHON LIBS" && \
     echo "======================" && \
     echo ""
-RUN pip install -U \
-      pip \
-      setuptools \
-      wheel
-RUN pip install \
+RUN pip install -U pip \
+    setuptools \
+    wheel \
+    virtualenv \
+    nose \
+    coverage \
+    codecov
+
+# This picks up the packages installed above, plus any of their
+# dependencies
+ENV DOCKER_PYTHON_CORE `pip list --format freeze | cut -d= -f1`
+RUN echo "DOCKER_PYTHON_CORE=${DOCKER_PYTHON_CORE}" && echo ""
+
+ENV DOCKER_PYTHON_OPTIONAL \
       sphinx \
       sphinx_rtd_theme \
-      virtualenv \
       cffi \
       numpy \
       mpi4py \
@@ -22,13 +30,31 @@ RUN pip install \
       openpyxl \
       pymysql \
       xlrd \
+      z3-solver \
       pint
+RUN pip install ${DOCKER_PYTHON_OPTIONAL}
+
 # These currently fail on PyPy
-RUN (python -c "import __pypy__" 2> /dev/null) || (pip install scipy matplotlib pandas seaborn)
-# These may not work on PyPy / Python3.7
+ENV DOCKER_PYTHON_NOT_PYPY \
+    scipy \
+    matplotlib \
+    pandas \
+    seaborn
+RUN (python -c "import __pypy__" 2> /dev/null) || (pip install ${DOCKER_PYTHON_NOT_PYPY})
+
+# These are fragile and may not work on PyPy / Python3.7
 RUN pip install PyYAML || \
     pip install https://github.com/yaml/pyyaml/archive/4.1.zip || \
     echo failed to install PyYAML
 RUN pip install numba || echo failed to install numba
 RUN pip install pyodbc || echo failed to install pyodbc
 RUN pip list
+
+# These are the Python packages that should be removed to return to a
+# "SLIM" build
+ENV DOCKER_PYTHON_SLIM \
+    ${DOCKER_PYTHON_OPTIONAL} \
+    ${DOCKER_PYTHON_NOT_PYPY} \
+    pyyaml \
+    numba \
+    pyodbc
