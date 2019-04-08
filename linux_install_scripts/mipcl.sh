aa1ba25097fc863d0aa32788bb2bdbcf78609e8d
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-04-08 19:41:01 -0400

    updating mipcl version

--- linux_install_scripts/mipcl.sh
+++ linux_install_scripts/mipcl.sh
@@ -3,7 +3,7 @@ RUN echo "" && \
     echo "INSTALLING MIPCL" && \
     echo "================" && \
     echo ""
-ENV MIPCL_VERSION="1.8.2"
+ENV MIPCL_VERSION="2.1.1"
 ARG TARGET="mipcl-${MIPCL_VERSION}"
 ENV PATH="${PREFIX}/${TARGET}/bin:${PATH}"
 RUN cd ${PREFIX} && \
