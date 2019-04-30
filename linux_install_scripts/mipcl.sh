704a8061eafa685f56bc2c65cf1e464decf723fd
John Siirola <jsiirola@users.noreply.github.com>
2019-04-30 12:50:03 -0600

    Updating the MIPCL version to 2.2.0

--- linux_install_scripts/mipcl.sh
+++ linux_install_scripts/mipcl.sh
@@ -3,7 +3,7 @@ RUN echo "" && \
     echo "INSTALLING MIPCL" && \
     echo "================" && \
     echo ""
-ENV MIPCL_VERSION="2.1.1"
+ENV MIPCL_VERSION="2.2.0"
 ARG TARGET="mipcl-${MIPCL_VERSION}"
 ENV PATH="${PREFIX}/${TARGET}/bin:${PATH}"
 RUN cd ${PREFIX} && \
