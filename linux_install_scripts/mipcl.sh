8612ed7b2697372eec8db75d535c6a84358f43b4
John Siirola <jsiirola@users.noreply.github.com>
2019-10-17 12:17:09 -0600

    Reverting MIPCL URL change

--- linux_install_scripts/mipcl.sh
+++ linux_install_scripts/mipcl.sh
@@ -8,7 +8,7 @@ ARG TARGET="mipcl-${MIPCL_VERSION}"
 ENV PATH="${PREFIX}/${TARGET}/bin:${PATH}"
 RUN cd ${PREFIX} && \
     rm -rf ${TARGET}.linux-x86_64.tar.gz && \
-    wget -q "http://www.mipcl-cpp.appspot.com/${TARGET}.linux-x86_64.tar.gz" && \
+    wget -q "http://www.mipcl-cpp.appspot.com/static/download/${TARGET}.linux-x86_64.tar.gz" && \
     tar xf ${TARGET}.linux-x86_64.tar.gz && \
     rm -rf ${TARGET}.linux-x86_64.tar.gz
 ARG TARGET
