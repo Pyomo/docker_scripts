931f4540ebf80b03dbb48a5c82a45a4df798f4e2
John Siirola <jsiirola@users.noreply.github.com>
2019-10-17 12:08:26 -0600

    Updating MIPCL download URL and current version

--- linux_install_scripts/mipcl.sh
+++ linux_install_scripts/mipcl.sh
@@ -3,12 +3,12 @@ RUN echo "" && \
     echo "INSTALLING MIPCL" && \
     echo "================" && \
     echo ""
-ENV MIPCL_VERSION="2.2.0"
+ENV MIPCL_VERSION="2.5.4"
 ARG TARGET="mipcl-${MIPCL_VERSION}"
 ENV PATH="${PREFIX}/${TARGET}/bin:${PATH}"
 RUN cd ${PREFIX} && \
     rm -rf ${TARGET}.linux-x86_64.tar.gz && \
-    wget -q "http://www.mipcl-cpp.appspot.com/static/download/${TARGET}.linux-x86_64.tar.gz" && \
+    wget -q "http://www.mipcl-cpp.appspot.com/${TARGET}.linux-x86_64.tar.gz" && \
     tar xf ${TARGET}.linux-x86_64.tar.gz && \
     rm -rf ${TARGET}.linux-x86_64.tar.gz
 ARG TARGET
