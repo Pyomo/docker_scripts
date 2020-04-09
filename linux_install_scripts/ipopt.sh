7474d68969336e8709a65026693132e3560f2da4
Miranda Mundt <mmundt@sandia.gov>
2020-04-09 12:55:04 -0600

    Adding -p to ensure that directories are forced to be created

--- linux_install_scripts/ipopt.sh
+++ linux_install_scripts/ipopt.sh
@@ -7,7 +7,7 @@ ARG TARGET="idaes-solvers-ubuntu1804-64"
 ENV IPOPT_VERSION="2.0.0"
 ARG IPOPT_DIR="IDAES-IPOPT-${IPOPT_VERSION}"
 ENV PATH="${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR}:${PATH}"
-RUN mkdir ${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR} && \
+RUN mkdir -p ${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR} && \
     cd ${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR} && \
     wget -q "https://github.com/IDAES/idaes-ext/releases/download/2.0.0/idaes-solvers-ubuntu1804-64.tar.gz" && \
     tar -xzf ${TARGET}.tar.gz && \
