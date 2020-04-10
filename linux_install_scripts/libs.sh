b667407f9ee135f0f3f06a27c9131cf52077279b
John Siirola <jsiirola@users.noreply.github.com>
2020-04-10 17:56:21 -0600

    Install recent cmake from source

--- linux_install_scripts/libs.sh
+++ linux_install_scripts/libs.sh
@@ -8,7 +8,7 @@ RUN dpkg --add-architecture i386 && \
     apt-get -q -y --no-install-recommends install \
         build-essential libssl-dev libffi-dev \
         wget zip unzip \
-        git subversion cmake \
+        git subversion \
         gfortran \
         libopenblas-dev \
         openmpi-bin openmpi-common libopenmpi-dev \
@@ -16,3 +16,8 @@ RUN dpkg --add-architecture i386 && \
         enchant \
         unixodbc unixodbc-dev && \
     rm -rf /var/lib/apt/lists/*
+# pypy2 is based on debian Jessie, which includes a very old cmake (3.0,
+# and ampl/mp requires 3.3).  We will install a new cmake binary
+RUN wget -q "https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.sh" -O install_cmake.sh && \
+    bash install_cmake.sh --skip-license --prefix=/ && \
+    rm install_cmake.sh
