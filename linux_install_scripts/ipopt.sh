7ebe0bb8f35a5f32d665383dae2946d3440705c9
John Siirola <jsiirola@users.noreply.github.com>
2020-04-10 01:53:38 -0600

    Rename IPOPT_VERSION -> IDAES_VERSION; attempt to run ipopt

--- linux_install_scripts/ipopt.sh
+++ linux_install_scripts/ipopt.sh
@@ -4,12 +4,13 @@ RUN echo "" && \
     echo "================" && \
     echo ""
 ARG TARGET="idaes-solvers-ubuntu1804-64"
-ENV IPOPT_VERSION="2.0.0"
-ARG IPOPT_DIR="IDAES-IPOPT-${IPOPT_VERSION}"
-ENV PATH="${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR}:${PATH}"
-RUN mkdir -p ${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR} && \
-    cd ${PREFIX}/IDAES_${IPOPT_VERSION}/${IPOPT_DIR} && \
+ENV IDAES_VERSION="2.0.0"
+ARG IPOPT_DIR="IDAES-IPOPT-${IDAES_VERSION}"
+ENV PATH="${PREFIX}/IDAES_${IDAES_VERSION}/${IPOPT_DIR}:${PATH}"
+RUN mkdir -p ${PREFIX}/IDAES_${IDAES_VERSION}/${IPOPT_DIR} && \
+    cd ${PREFIX}/IDAES_${IDAES_VERSION}/${IPOPT_DIR} && \
     wget -q "https://github.com/IDAES/idaes-ext/releases/download/2.0.0/idaes-solvers-ubuntu1804-64.tar.gz" && \
     tar -xzf ${TARGET}.tar.gz && \
     rm -rf ${TARGET}.tar.gz
+RUN ${PREFIX}/IDAES_${IDAES_VERSION}/ipopt
 ARG TARGET
