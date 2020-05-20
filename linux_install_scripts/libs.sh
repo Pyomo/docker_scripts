8b51706197a16002dc92031ee9dba3a04443540c
Miranda Mundt <mmundt@sandia.gov>
2020-05-20 12:34:43 -0600

    Moving CBC back to the end - adding back removed libraries

--- linux_install_scripts/libs.sh
+++ linux_install_scripts/libs.sh
@@ -3,17 +3,18 @@ RUN echo "" && \
     echo "INSTALLING COMMON LIBS" && \
     echo "======================" && \
     echo ""
-RUN apt-get -q update && \
+RUN dpkg --add-architecture i386 && \
+    apt-get -q update && \
     apt-get -q -y --no-install-recommends install \
         build-essential libssl-dev libffi-dev \
         wget zip unzip \
         git subversion cmake \
-        gfortran glpk-utils \
+        gfortran \
         libgfortran4 \
         libopenblas-dev \
         liblapack-dev \
         openmpi-bin openmpi-common libopenmpi-dev \
-        libc6 libncurses5 libstdc++6 \
+        libc6:i386 libncurses5:i386 libstdc++6:i386 \
         enchant \
         unixodbc unixodbc-dev && \
     rm -rf /var/lib/apt/lists/*
