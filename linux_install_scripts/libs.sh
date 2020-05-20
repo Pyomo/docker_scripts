b7d7ed5e51d10f901631841ad1333cfe1956df4c
Miranda Mundt <mmundt@sandia.gov>
2020-05-20 11:44:02 -0600

    Not sure why the system libraries can be found for some versions but not others

--- linux_install_scripts/libs.sh
+++ linux_install_scripts/libs.sh
@@ -3,18 +3,17 @@ RUN echo "" && \
     echo "INSTALLING COMMON LIBS" && \
     echo "======================" && \
     echo ""
-RUN dpkg --add-architecture i386 && \
-    apt-get -q update && \
+RUN apt-get -q update && \
     apt-get -q -y --no-install-recommends install \
         build-essential libssl-dev libffi-dev \
         wget zip unzip \
         git subversion cmake \
         gfortran \
-        gcc-7 g++-7 \
+        libgfortran4 \
         libopenblas-dev \
         liblapack-dev \
         openmpi-bin openmpi-common libopenmpi-dev \
-        libc6:i386 libncurses5:i386 libstdc++6:i386 \
+        libc6 libncurses5 libstdc++6 \
         enchant \
         unixodbc unixodbc-dev && \
     rm -rf /var/lib/apt/lists/*
